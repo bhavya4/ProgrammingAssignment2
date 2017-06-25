bf484fdbb27dc587ec8b951463e3a9ef4b2a63d2
bhavya4 <bhavya4.mishra@gmail.com>
2017-06-26 00:26:09 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a function that stores a recently computed inverse of a
+## matrix in a cache memory. it returns it whenever the same matrix is sent again
+## If a new matrix is sent in this function, then it clears the cache memory to
+## to store the new inverse matrix.
 
-## Write a short comment describing this function
+## This function uses the feature of lexical scoping in R to make changes to 
+## to m and x. It does not calculate in inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) m <<- inv
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve uses Solve() funtion in R to calculate the inverse of a matrix if
+## the matrix is not alreasy stored in cache. it has access to all functions
+## in cacheMakeMatrix because of the list that gets passed on to it. The list 
+##contains pointers to the parent environment through the functions.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    print("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  if(nrow(data)==ncol(data))
+  {
+    m<-solve(data)
+  }
+  else
+  {
+    l<-svd(data)
+    m<-l$v%*%diag(1/l$d)%*%t(l$u)
+  }
+  x$setInverse(m)
+  m
+  
 }
